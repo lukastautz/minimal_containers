9010461753341ab7cbe912249c3a665297b69a61
Lukas Tautz <104827162+lukastautz@users.noreply.github.com>
2024-08-02 14:56:30 +0200

    Update container.c

--- container.c
+++ container.c
@@ -1,3 +1,21 @@
+/*
+Copyright 2024 Lukas Tautz
+
+This file is part of minimal_containers.
+
+minimal_containers is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, version 3 of the License.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 #ifdef __dietlibc__
 #define _GNU_SOURCE /* for u_intN_t + sched.h */
 #include <sched.h>
